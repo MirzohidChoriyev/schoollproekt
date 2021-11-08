e2bdc39abd07ceb9078a550e334db66e1b0da6f8
MirzohidChoriyev <90138286+MirzohidChoriyev@users.noreply.github.com>
2021-11-08 16:51:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react'
 function App() {
   return (
     <div>
+<<<<<<< Updated upstream
         
+=======
+         proekt maktab  
+         Mirzohid  && Bekzod
+         <h1>Lets go</h1>
+>>>>>>> Stashed changes
     </div>
   )
 }
