3f4719578fe865a40d7b3a9fe24cf671c5747b1d
Bekzod <75493469+MrBekzod0899@users.noreply.github.com>
2021-11-09 18:40:20 +0500

    I added new files and dependesies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import NavbarHeader1 from './Components/Navbar/Navbar'
+import Header from './Components/Header/Header'
+import {Routes,Route,BrowserRouter,Switch} from "react-router-dom"
+import Content from './Components/Content/Content'
+import Footer from './Components/Footer/Footer'
+import SchoolLive from './Components/MaktabHayoti/SchoolLive'
+import Contact from './Components/Contact/Contact'
+import Darsliklar from './Components/Darsliklar/Darsliklar'
 
-function App() {
-  return (
-    <div>
-        
-    </div>
-  )
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Switch>
+                  <Route exact path="/">
+                        <NavbarHeader1/>
+                        <Content/>
+                        <Contact/>
+                        <Darsliklar/>
+                        <Footer/>
+                  </Route>
+                  <Route exact path="/maktab-hayoti">
+                    <NavbarHeader1/>
+                    <SchoolLive/>
+                    <Footer/>
+                  </Route>
+          </Switch>
+
+      </div>
+    )
+  }
 }
-
-export default App
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Route } from 'react-router-dom';
+import 'aos'
+import "aos/dist/aos.css";
+import '@mui/material'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+       <App/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
